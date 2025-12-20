f051660a81117e00ba88dbae365a8d16b2c2c0a5
binhson <sonbinh160958@gmail.com>
2025-12-20 16:17:31 +0700

    feat: update arduino

--- Assets/Arduino/ArduinoController/ArduinoController.ino
+++ Assets/Arduino/ArduinoController/ArduinoController.ino
@@ -1,9 +1,10 @@
 const int leds[] = {2, 3, 4, 5, 6};
 const int buttons[] = {8, 9, 10, 11, 12};
 const int ledCount = 5;
-const int ledL = 13;
 
-int currentIndex = -1;
+bool ledState[5] = {false};
+unsigned long lastPressTime[5] = {0};
+const unsigned long debounceDelay = 200;
 
 void setup() {
   Serial.begin(9600);
@@ -11,52 +12,36 @@ void setup() {
   for (int i = 0; i < ledCount; i++) {
     pinMode(leds[i], OUTPUT);
     pinMode(buttons[i], INPUT_PULLUP);
+    digitalWrite(leds[i], LOW);
   }
-  pinMode(ledL, OUTPUT);
 }
 
 void loop() {
   if (Serial.available()) {
     String data = Serial.readStringUntil('\n');
-    currentIndex = data.toInt();
+    data.trim();
+    int index = data.toInt();
 
-    for (int i = 0; i < ledCount; i++) {
-      digitalWrite(leds[i], LOW);
+    if (index == -1) {
+      for (int i = 0; i < ledCount; i++) {
+        ledState[i] = false;
+        digitalWrite(leds[i], LOW);
+      }
     }
-    digitalWrite(ledL, LOW);
-
-    if (currentIndex >= 0 && currentIndex < ledCount) {
-      digitalWrite(leds[currentIndex], HIGH);
+    else if (index >= 0 && index < ledCount) {
+      ledState[index] = !ledState[index];
+      digitalWrite(leds[index], ledState[index] ? HIGH : LOW);
     }
   }
 
-  // if (currentIndex != -1) {
-  //   if (digitalRead(buttons[currentIndex]) == LOW) {
-  //     digitalWrite(ledL, HIGH);
-  //     Serial.print("HIT:");
-  //     Serial.println(currentIndex);
-  //     delay(300); 
-  //   }
-  // }
-
-  if (digitalRead(buttons[0]) == LOW) {
-    Serial.println("HIT:0");
-    delay(200);
-  }
-  if (digitalRead(buttons[1]) == LOW) {
-    Serial.println("HIT:1");
-    delay(200);
-  }
-
-  if (digitalRead(buttons[2]) == LOW) {
-    Serial.println("HIT:2");
-    delay(200);
-  }
-  if (digitalRead(buttons[3]) == LOW) {
-    Serial.println("HIT:3");
-    delay(200);
-  }if (digitalRead(buttons[4]) == LOW) {
-    Serial.println("HIT:4");
-    delay(200);
+  for (int i = 0; i < ledCount; i++) {
+    if (digitalRead(buttons[i]) == LOW) {
+      unsigned long now = millis();
+      if (now - lastPressTime[i] > debounceDelay) {
+        lastPressTime[i] = now;
+        Serial.print("HIT:");
+        Serial.println(i);
+      }
+    }
   }
 }
